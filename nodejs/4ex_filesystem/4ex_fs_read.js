b3df092506f6accd3d44cb2c7dc3637db7d5b49a
Kimjw111 <whale960105@gmail.com>
2023-06-21 13:43:53 +0900

    nodejs fs

--- nodejs/4ex_filesystem/4ex_fs_read.js
+++ nodejs/4ex_filesystem/4ex_fs_read.js
@@ -7,16 +7,22 @@ const app=express()
 const PORT=3000
 const _path=path.join(__dirname,'/')
 // console.log(_path)
-// app.use('/',express.static(_path))
+app.use('/',express.static(_path))
 app.use(logger('tiny'))
 
 app.get('/files',(req,res)=>{
     fs.readdir(_path,'utf-8',(err,data)=>{
         //경로, 파일 형식, 콜백(에러났을때, 실제로 받을 데이터)
-        let list=''
-      data.reduce((a,c,i,arr)=>{
-        list+=`<h4 style="color:plum ;">${c}</h4>`
-      })
+        // console.log(Array.isArray(data))
+        let list='<ul>'
+        data.forEach(v=>{
+          if(v.indexOf('.')===-1){
+            list+=`<li><a href="#">[${v}]</a></li>`
+          }else{
+            list+=`<li><a href="/${v}" download>${v}</a></li>`
+          }
+        })
+      list+='</ul>'
         res.send(list)
     })
 })
---
+++ nodejs/4ex_filesystem/4ex_fs_write2.js
@@ -0,0 +1,41 @@
+const express=require('express')
+const path=require('path')
+const logger=require('morgan')
+const fs=require('fs')
+const app=express()
+const _path=path.join(__dirname,'/')
+app.use('/',express.static(_path))
+app.use(logger('tiny'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.post('/data',(req,res)=>{
+    const obj=req.body
+    fs.writeFile(_path+obj.name+'.txt',JSON.stringify(obj),(e)=>{
+    if(e)console.log(e)
+    console.log('파일 작성이 완료되었습니다.')
+    res.send(`<script>alert('${obj.name}파일로 저장합니다.');history.go(-1)</script>`)
+})
+})
+app.get('/list',(req,res)=>{
+    fs.readdir(_path,'utf-8',(err,data)=>{
+        let list='<ul>'
+        data.forEach(v=>{
+          if(v.indexOf('.')===-1){
+            list+=`<li><a href="#">[${v}]</a></li>`
+          }else{
+            list+=`<li><a href="/${v}">${v}</a></li>`
+          }
+        })
+      list+='</ul><a href="./index.html">홈으로</a>'
+        res.send(list)
+    }) 
+})
+fs.appendFile(_path+'test1'+'.txt','test will append',(e)=>{
+        if(e)console.log(e)
+        console.log('append')
+    })
+
+
+app.listen(3000,()=>{console.log('listening on port 3000')})
