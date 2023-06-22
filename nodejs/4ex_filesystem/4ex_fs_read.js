37850ebf97f8ffd6fa2b12102c276551ca58e408
Kimjw111 <whale960105@gmail.com>
2023-06-22 17:21:24 +0900

    nodejs fs

--- nodejs/4ex_filesystem/4ex_fs_read.js
+++ nodejs/4ex_filesystem/4ex_fs_read.js
@@ -1,33 +1,44 @@
-const express=require('express')
-const path=require('path')
-const logger=require('morgan')
-const fs=require('fs')
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const fs = require('fs')
 
-const app=express()
-const PORT=3000
-const _path=path.join(__dirname,'/')
+const app = express()
+const PORT = 3000
+const _path = path.join(__dirname, '/')
 // console.log(_path)
-app.use('/',express.static(_path))
+app.use('/', express.static(_path))
 app.use(logger('tiny'))
 
-app.get('/files',(req,res)=>{
-    fs.readdir(_path,'utf-8',(err,data)=>{
-        //경로, 파일 형식, 콜백(에러났을때, 실제로 받을 데이터)
-        // console.log(Array.isArray(data))
-        let list='<ul>'
-        data.forEach(v=>{
-          if(v.indexOf('.')===-1){
-            list+=`<li><a href="#">[${v}]</a></li>`
-          }else{
-            list+=`<li><a href="/${v}" download>${v}</a></li>`
-          }
-        })
-      list+='</ul>'
-        res.send(list)
+app.get('/files', (req, res) => {
+  fs.readdir(_path, 'utf-8', (err, data) => {
+    //경로, 파일 형식, 콜백(에러났을때, 실제로 받을 데이터)
+    // console.log(Array.isArray(data))
+    let list = '<ul>'
+    data.forEach((v) => {
+      if (v.indexOf('.') === -1) {
+        list += `<li><a href="#">[${v}]</a></li>`
+      } else {
+        list += `<li><a href="/${v}">${v}</a><button onclick="location.href='del/${v}'">삭제</button></li>`
+      }
     })
+    list += '</ul>'
+    res.send(list)
+  })
 })
-
-
-app.listen(PORT,()=>{
-    console.log('listening on port'+PORT)
+app.get('/del/:fname', (req, res) => {
+  const fname = req.params.fname
+  fs.unlink(_path + '/' + fname, (err) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('삭제를 성공하였습니다.')
+  })
+  res.send(
+    `<script>alert('${fname}을 삭제하였습니다.');location.href=document.referrer</script>`
+  )
+})
+app.listen(PORT, () => {
+  console.log('listening on port' + PORT)
 })
--- nodejs/4ex_filesystem/4ex_fs_write2.js
+++ nodejs/4ex_filesystem/4ex_fs_write2.js
@@ -1,41 +1,47 @@
-const express=require('express')
-const path=require('path')
-const logger=require('morgan')
-const fs=require('fs')
-const app=express()
-const _path=path.join(__dirname,'/')
-app.use('/',express.static(_path))
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const fs = require('fs')
+const app = express()
+const _path = path.join(__dirname, '/')
+app.use('/', express.static(_path))
 app.use(logger('tiny'))
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.post('/data',(req,res)=>{
-    const obj=req.body
-    fs.writeFile(_path+obj.name+'.txt',JSON.stringify(obj),(e)=>{
-    if(e)console.log(e)
+app.post('/data', (req, res) => {
+  const obj = req.body
+  fs.writeFile(_path + obj.name + '.txt', JSON.stringify(obj), (e) => {
+    if (e) console.log(e)
     console.log('파일 작성이 완료되었습니다.')
-    res.send(`<script>alert('${obj.name}파일로 저장합니다.');history.go(-1)</script>`)
+    res.send(
+      `<script>alert('${obj.name}파일로 저장합니다.');history.go(-1)</script>`
+    )
+  })
 })
-})
-app.get('/list',(req,res)=>{
-    fs.readdir(_path,'utf-8',(err,data)=>{
-        let list='<ul>'
-        data.forEach(v=>{
-          if(v.indexOf('.')===-1){
-            list+=`<li><a href="#">[${v}]</a></li>`
-          }else{
-            list+=`<li><a href="/${v}">${v}</a></li>`
-          }
-        })
-      list+='</ul><a href="./index.html">홈으로</a>'
-        res.send(list)
-    }) 
-})
-fs.appendFile(_path+'test1'+'.txt','test will append',(e)=>{
-        if(e)console.log(e)
-        console.log('append')
+app.get('/list', (req, res) => {
+  fs.readdir(_path, 'utf-8', (err, data) => {
+    let list = '<ul>'
+    data.forEach((v) => {
+      if (v.indexOf('.') === -1) {
+        list += `<li><a href="#">[${v}]</a></li>`
+      } else {
+        list += `<li><a href="/${v}">${v}</a></li>`
+      }
     })
-
-
-app.listen(3000,()=>{console.log('listening on port 3000')})
+    list += '</ul><a href="./index.html">홈으로</a>'
+    res.send(list)
+  })
+})
+fs.appendFile(_path + 'test1' + '.txt', 'test will append', (e) => {
+  if (e) console.log(e)
+  console.log('append')
+})
+fs.unlink(_path + 'test1' + '.txt', (err) => {
+  if (err) console.log(err)
+  console.log('file is delete')
+})
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
