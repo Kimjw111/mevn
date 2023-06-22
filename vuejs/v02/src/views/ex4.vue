95763e489b49460402b423397eeb552873577f80
Kimjw111 <whale960105@gmail.com>
2023-06-22 12:41:25 +0900

    vue

--- vuejs/v02/src/views/ex4.vue
+++ vuejs/v02/src/views/ex4.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>EX4</div>
+  <div>
+    <button v-for="p in pages" :key="p" :class="p" @click="page = p">
+      {{ p }}
+    </button>
+    <div v-for="(g, i) in pages" :key="i">
+      <img v-if="g === page" :src="'./sample' + (i + 1) + '.jpg'" alt="사진" />
+    </div>
+  </div>
 </template>
-<script></script>
-<style></style>
+<script>
+export default {
+  data() {
+    return {
+      page: 'page1',
+      pages: ['page1', 'page2', 'page3', 'page4']
+    }
+  }
+}
+</script>
+<style>
+button {
+  scale: 200%;
+  background-color: white;
+  border: 1px solid gray;
+  margin: 30px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: yellow;
+}
+button:active {
+  color: white;
+  background-color: #380d68;
+}
+img {
+  height: 50%;
+  width: 50%;
+}
+</style>
---
+++ vuejs/v03/src/views/Select.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <select name="" id="" v-model="selData">
+      <option v-for="i in arr" :key="i" :value="i.code">{{ i.city }}</option>
+    </select>
+    <span>{{ selData }}</span>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      selData: '고르세요.',
+      arr: [
+        { city: '부산', code: '051' },
+        { city: '서울', code: '02' },
+        { city: '제주', code: '064' },
+        { city: '창원', code: '055' },
+        { city: '대구', code: '053' }
+      ]
+    }
+  }
+}
+</script>
+<style></style>
