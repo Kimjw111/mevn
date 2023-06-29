d130e705e4a3dc62c5ba05f0f67234587d37810f
Kimjw111 <whale960105@gmail.com>
2023-06-29 17:18:12 +0900

    서버프로그램구현 시험

--- 서버프로그램구현_김정우/app.js
+++ 서버프로그램구현_김정우/app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const logger = require('morgan')
 const fs = require('fs')
 const app = express()
-const _path = path.join(__dirname, '/')
+const _path = path.join(__dirname, './dist')
 const PORT = 3000
 app.use('/', express.static(_path))
 app.use(logger('tiny'))
@@ -13,7 +13,9 @@ app.use(express.urlencoded({ extended: true }))
 
 app.post('/data', (req, res) => {
   const obj = req.body
-  fs.writeFile(_path + obj.id + '.txt', JSON.stringify(obj), (e) => {
+  console.log(obj)
+  console.log(obj.id)
+  fs.writeFile(_path + '/' + obj.id + '.txt', JSON.stringify(obj), (e) => {
     if (e) console.log(e)
     console.log('파일 작성이 완료되었습니다.')
     res.send(
